5e74096c19aaf27978ca045cd90e17887c8efcf9
Guillaume Plante <tigui.plante@icloud.com>
2025-03-03 20:35:55 -0500

    version script and added debug release buids

---
+++ scripts/build.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+
+#+--------------------------------------------------------------------------------+
+#|                                                                                |
+#|   build.sh                                                                  |
+#|                                                                                |
+#+--------------------------------------------------------------------------------+
+#|   Guillaume Plante <codegp@icloud.com>                                         |
+#|   Code licensed under the GNU GPL v3.0. See the LICENSE file for details.      |
+#+--------------------------------------------------------------------------------+
+
+# variables for colors
+WHITE='\033[0;97m'
+CYAN='\033[0;36m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+
+
+SCRIPT_PATH=$(realpath "$BASH_SOURCE")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+tmp_root=$(pushd "$SCRIPT_DIR/.." | awk '{print $1}')
+ROOT_DIR=$(eval echo "$tmp_root")
+ENV_FILE="$ROOT_DIR/.env"
+ROOT_DIRECTORY="$ROOT_DIR"
+SCRIPT_DIR="$ROOT_DIR/scripts"
+BIN_DIR="$ROOT_DIR/bin"
+BIN_OUT="$BIN_DIR/sysnotify"
+LOGS_DIR="$ROOT_DIR/logs"
+LOG_FILE="$LOGS_DIR/build.log"
+VERSION_FILE=$ROOT_DIR/version.nfo
+BUILD_FILE=$ROOT_DIR/build.nfo
+
+
+
+# =========================================================
+# function:     logs functions
+# description:  log messages to fils and console
+#
+# =========================================================
+log_ok() {
+    if [[ -f "$LOG_FILE" ]]; then
+        echo "[$(date)] $1" >> "$LOG_FILE"
+    fi
+    echo -e " ✔️ ${GREEN}[SUCCESS]${NC} ${CYAN}$1${NC}"
+}
+log_info() {
+    if [[ -f "$LOG_FILE" ]]; then
+        echo "[$(date)] $1" >> "$LOG_FILE"
+    fi
+    echo -e " ⚡ ${CYAN}[log]${NC} $1"
+}
+log_warn() {
+    if [[ -f "$LOG_FILE" ]]; then
+        echo "[$(date)] $1" >> "$LOG_FILE"
+    fi
+    echo -e " ⚠️ ${YELLOW}[warn]${NC} ${WHITE}$1${NC}"
+}
+log_error() {
+    if [[ -f "$LOG_FILE" ]]; then
+        echo "[$(date)] $1" >> "$LOG_FILE"
+    fi
+    echo -e " ❌ ${RED}[error]${NC} ${YELLOW}$1${NC}"
+    exit 1
+}
+
+pushd "$ROOT_DIR" > /dev/null
+
+# Create new build directory
+mkdir -p "$BIN_DIR"
+mkdir -p "$LOGS_DIR"
+
+
+TARGET_TYPE="Release"
+
+if [[ $# -eq 1 && ! "$1" == "debug" ]]; then
+    TARGET_TYPE="Debug"
+fi
+
+# Check if cmake is installed
+if ! command -v cmake &> /dev/null; then
+    log_error "Error: CMake is not installed. Please install it and try again."
+    exit 1
+fi
+
+# Remove existing build directory
+if [ -d "$BIN_DIR" ]; then
+    log_warn "Cleaning existing build directory..."
+    rm -rf "$BIN_DIR"
+fi
+
+
+# Generate project files
+log_info "Generating project files in $TARGET_TYPE mode..."
+cmake -DCMAKE_BUILD_TYPE=$TARGET_TYPE .
+
+# Build the project
+log_info "Building project in $TARGET_TYPE mode..."
+make release
+
+if [[ $? -ne 0 ]]; then
+    log_error "build failed!"
+    exit 1
+fi
+
+
+# Remove existing build directory
+if [ ! -f "$BIN_OUT" ]; then
+    log_error "build failed, missing $BIN_OUT"
+    exit 1
+fi
+
+log_ok "Build completed successfully."
+popd > /dev/null
